ebfbad4b1be0d67fedeb9b23ea9b0d984404d574
dgalnykin <developer085@gmail.com>
2024-11-28 21:24:08 +0200

    generate backend apps

---
+++ apps/lanygina-elena-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'lanygina-elena-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/lanygina-elena-api'
+};
