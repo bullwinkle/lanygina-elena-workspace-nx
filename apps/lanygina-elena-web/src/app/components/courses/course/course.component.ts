8b30150c03c6c8152734214f19658a503f92eea4
dgalnykin <developer085@gmail.com>
2024-11-29 01:36:45 +0200

    test

--- apps/lanygina-elena-web/src/app/components/courses/course/course.component.ts
+++ apps/lanygina-elena-web/src/app/components/courses/course/course.component.ts
@@ -1,12 +1,11 @@
 import {Component, Input} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
-import {CourseDetailsComponent} from '../details/course-details.component';
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, CourseDetailsComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './course.component.html',
   styleUrl: './course.component.css',
 })
