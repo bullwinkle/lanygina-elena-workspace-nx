b5e36be409e153f711eb44213f7003ed40a3e39f
dgalnykin <developer085@gmail.com>
2024-11-29 04:30:24 +0200

    telegram-bot uses api

--- apps/lanygina-elena-telegram-bot/src/main.ts
+++ apps/lanygina-elena-telegram-bot/src/main.ts
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 app.get('/api', async (req, res) => {
   try {
-    const response = await fetch(`${BACKEND_URL}/hello`);
+    const response = await fetch(`${BACKEND_URL}`);
     const data = await response.json();
     res.send(data);
   } catch (error) {
